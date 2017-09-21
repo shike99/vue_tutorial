89a3737c54c13209662c2907ff47bc43bce6e06f
ken0909 <shiva0909ken@gmail.com>
2017-09-21 18:31:33 +0900

    done tutorial phase one

--- src/main.js
+++ src/main.js
@@ -1,13 +1,44 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import Hello from './components/Hello';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>',
+});
+
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App },
+  data: {
+    message: 'message',
+    message2: `You loaded this page on ${new Date().toLocaleString()}`,
+    seen: true,
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' },
+    ],
+    name: 'orange',
+    price: 200,
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('');
+    },
+  },
+  components: {
+    Hello,
+  },
+  computed: {
+    priceWithTax() {
+      return this.price * 1.08;
+    },
+    isValid() {
+      return this.message.length > 0;
+    },
+  },
 });
